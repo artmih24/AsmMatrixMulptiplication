32674e2363e4eeae62064f957633627498aeac27
Artem Mikhailovič <83209415+artmih24@users.noreply.github.com>
2022-04-04 19:55:55 +0300

    Add files via upload

--- get_results.sh
+++ get_results.sh
@@ -4,67 +4,33 @@
 
 if [ $# -eq 1 ] 
 then
-    Mconst=$1
-    Nconst=$1
-    Kconst=$1
-    start=8
-    step=8
-    end=8192
+    Mconst=$1; Nconst=$1; Kconst=$1; start=8; step=8; end=8192
 elif [ $# -eq 2 ] 
 then
-    Mconst=$1
-    Nconst=$1
-    Kconst=$1
-    start=$2
-    step=$2
-    end=8192
+    Mconst=$1; Nconst=$1; Kconst=$1; start=$2; step=$2; end=8192
 elif [ $# -eq 3 ] 
 then
-    Mconst=$1
-    Nconst=$2
-    Kconst=$3
-    start=8
-    step=8
-    end=8192
+    Mconst=$1; Nconst=$2; Kconst=$3; start=8; step=8; end=8192
 elif [ $# -eq 4 ] 
 then
-    Mconst=$1
-    Nconst=$2
-    Kconst=$3
-    start=$4
-    step=$4
-    end=8192
+    Mconst=$1; Nconst=$2; Kconst=$3; start=$4; step=$4; end=8192
 elif [ $# -eq 5 ] 
 then
-    Mconst=$1
-    Nconst=$2
-    Kconst=$3
-    start=$4
-    step=$5
-    end=8192
+    Mconst=$1; Nconst=$2; Kconst=$3; start=$4; step=$5; end=8192
 elif [ $# -eq 6 ] 
 then
-    Mconst=$1
-    Nconst=$2
-    Kconst=$3
-    start=$4
-    step=$5
-    end=$6
+    Mconst=$1; Nconst=$2; Kconst=$3; start=$4; step=$5; end=$6
 else
-    Mconst=128
-    Nconst=128
-    Kconst=128
-    start=8
-    step=8
-    end=8192
+    Mconst=128; Nconst=128; Kconst=128; start=8; step=8; end=8192
+    echo "Default values"
     if [ $# -ne 0 ] 
     then
         echo "Wrong number of arguments!"
     fi
 fi
 
-echo "const M="$Mconst "N="$Nconst "K="$Kconst
-echo "start="$start "step="$step "end="$end
+echo "const M = "$Mconst", N = "$Nconst", K = "$Kconst
+echo "start = "$start", step = "$step", end = "$end
 
 > 'results_M.csv'
 > 'results_N.csv'
--- gnuplot_commands.plt
+++ gnuplot_commands.plt
@@ -1,13 +1,25 @@
 #!/usr/local/bin/gnuplot --persist
 
 #set multiplot layout 2,2 rowsfirst
-end=ARG1
-step=ARG2
-if (!exists(ARG1)) end=8192
-if (!exists(ARG2)) step=256
-#print end
+end = ARG1
+step = ARG2
+if (end eq "") {
+     end = 8192
+     print "default end = ", end
+}
+else {
+     print "end = ", end
+}
+if (step eq "") {
+     step = 256
+     print "default step = ", step
+}
+else {
+     print "step = ", step
+}
 set xtics 0,step,end
 set ytics 0,5,100
+#set yrange [0:100]
 set grid
 set datafile separator ","
 plot "results_M.csv" using 1:2 with lines lt rgb "red" title "Performance(M)", \
