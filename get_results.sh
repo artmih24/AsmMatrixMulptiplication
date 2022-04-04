38c4088984cd9015443443e8a55ce8ddd98e6adf
Artem Mikhailovič <83209415+artmih24@users.noreply.github.com>
2022-04-04 15:01:38 +0300

    Add files via upload

--- get_results.sh
+++ get_results.sh
@@ -2,23 +2,87 @@
 
 #./compile.sh
 
+if [ $# -eq 1 ] 
+then
+    Mconst=$1
+    Nconst=$1
+    Kconst=$1
+    start=8
+    step=8
+    end=8192
+elif [ $# -eq 2 ] 
+then
+    Mconst=$1
+    Nconst=$1
+    Kconst=$1
+    start=$2
+    step=$2
+    end=8192
+elif [ $# -eq 3 ] 
+then
+    Mconst=$1
+    Nconst=$2
+    Kconst=$3
+    start=8
+    step=8
+    end=8192
+elif [ $# -eq 4 ] 
+then
+    Mconst=$1
+    Nconst=$2
+    Kconst=$3
+    start=$4
+    step=$4
+    end=8192
+elif [ $# -eq 5 ] 
+then
+    Mconst=$1
+    Nconst=$2
+    Kconst=$3
+    start=$4
+    step=$5
+    end=8192
+elif [ $# -eq 6 ] 
+then
+    Mconst=$1
+    Nconst=$2
+    Kconst=$3
+    start=$4
+    step=$5
+    end=$6
+else
+    Mconst=128
+    Nconst=128
+    Kconst=128
+    start=8
+    step=8
+    end=8192
+    if [ $# -ne 0 ] 
+    then
+        echo "Wrong number of arguments!"
+    fi
+fi
+
+echo "const M="$Mconst "N="$Nconst "K="$Kconst
+echo "start="$start "step="$step "end="$end
+
 > 'results_M.csv'
 > 'results_N.csv'
 > 'results_K.csv'
 
-for i in {8..8192..8}
+for (( i=$start; i<=$end; i+=$step ))
 do
-    echo $i","$(./a.out --increment $i 128 128) >> 'results_M.csv'
+    echo $i","$(./a.out --increment $i $Nconst $Kconst) >> 'results_M.csv'
 done
 
-for i in {8..8192..8}
+for (( i=$start; i<=$end; i+=$step ))
 do
-    echo $i","$(./a.out --increment 128 $i 128) >> 'results_N.csv'
+    echo $i","$(./a.out --increment $Mconst $i $Kconst) >> 'results_N.csv'
 done
 
-for i in {8..8192..8}
+for (( i=$start; i<=$end; i+=$step ))
 do
-    echo $i","$(./a.out --increment 128 128 $i) >> 'results_K.csv'
+    echo $i","$(./a.out --increment $Mconst $Nconst $i) >> 'results_K.csv'
 done
 
-gnuplot gnuplot_commands.plt
+gnuplot -c gnuplot_commands.plt $end $(($end/32))
--- gnuplot_commands.plt
+++ gnuplot_commands.plt
@@ -1,5 +1,12 @@
+#!/usr/local/bin/gnuplot --persist
+
 #set multiplot layout 2,2 rowsfirst
-set xtics 0,256,8192
+end=ARG1
+step=ARG2
+if (!exists(ARG1)) end=8192
+if (!exists(ARG2)) step=256
+#print end
+set xtics 0,step,end
 set ytics 0,5,100
 set grid
 set datafile separator ","
